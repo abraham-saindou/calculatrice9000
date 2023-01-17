97c49518c19ce4b48e9e984e1370817c79c8aaa1
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-01-17 20:48:01 +0100

    calculator can be used

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# importing tkinter library
 from tkinter import *
 
-
+# setting parameters of the user interface
 root = Tk()
 root.config(background='white')
 root.resizable(width=False, height=False)
@@ -8,17 +9,19 @@ root.geometry("480x568+450+90")
 root.title('Calculatrice')
 root.geometry("800x600+20+20")
 
-numericEq = StringVar()
-calcValue = ""
+
+numericEq = StringVar()  #
+calcValue = "" # value storing
 dataField = Entry(root, textvariable=numericEq, font="Serif 15")
 dataField.grid(row=0,columnspan=3, ipadx=80, ipady=15)
 
 def btn_click(x):
     global calcValue
-    calcValue = calcValue + str(x)
+    calcValue = calcValue + str(x) #str() converts int type into string, then add is added to calcValue
     numericEq.set(calcValue)
 
 def clearInput():
+    global calcValue
     calcValue = ""
     numericEq.set("")
 
@@ -28,26 +31,26 @@ def btn_equal():
     total = str(somme)
     numericEq.set(total)
 
-button1 = Button(root, text="1", width=5, height=3, command=btn_click(1))
-button2 = Button(root, text="2", width=5, height=3, command=btn_click(2))
-button3 = Button(root, text="3", width=5, height=3, command=btn_click(3))
-button4 = Button(root, text="4", width=5, height=3, command=btn_click(4))
-button5 = Button(root, text="5", width=5, height=3, command=btn_click(5))
-button6 = Button(root, text="6", width=5, height=3, command=btn_click(6))
-button7 = Button(root, text="7", width=5, height=3, command=btn_click(7))
-button8 = Button(root, text="8", width=5, height=3, command=btn_click(8))
-button9 = Button(root, text="9", width=5, height=3, command=btn_click(9))
-button0 = Button(root, text="0", width=10, height=3, command=btn_click(0))
-button_dot = Button(root, text=".", width=5, height=3, command=btn_click("."))
+button1 = Button(root, text="1", width=5, height=3, command=lambda: btn_click(1))
+button2 = Button(root, text="2", width=5, height=3, command=lambda: btn_click(2))
+button3 = Button(root, text="3", width=5, height=3, command=lambda: btn_click(3))
+button4 = Button(root, text="4", width=5, height=3, command=lambda: btn_click(4))
+button5 = Button(root, text="5", width=5, height=3, command=lambda: btn_click(5))
+button6 = Button(root, text="6", width=5, height=3, command=lambda: btn_click(6))
+button7 = Button(root, text="7", width=5, height=3, command=lambda: btn_click(7))
+button8 = Button(root, text="8", width=5, height=3, command=lambda: btn_click(8))
+button9 = Button(root, text="9", width=5, height=3, command=lambda: btn_click(9))
+button0 = Button(root, text="0", width=10, height=3, command=lambda: btn_click(0))
+button_dot = Button(root, text=".", width=5, height=3, command=lambda: btn_click("."))
 
-button_plus = Button(root, text="+", width=5, height=7, command=btn_click("+"))
-button_less = Button(root, text="-", width=5, height=3, command=btn_click("-"))
-button_mult = Button(root, text="*", width=5, height=3, command=btn_click("*"))
-button_div = Button(root, text="/", width=5, height=3, command=btn_click("/"))
+button_plus = Button(root, text="+", width=5, height=7, command=lambda: btn_click("+"))
+button_less = Button(root, text="-", width=5, height=3, command=lambda: btn_click("-"))
+button_mult = Button(root, text="*", width=5, height=3, command=lambda: btn_click("*"))
+button_div = Button(root, text="/", width=5, height=3, command=lambda: btn_click("/"))
 
-button_mod = Button(root, text="%", width=5, height=3, command=btn_click("%"))
-button_sum = Button(root, text="=", width=5, height=6, command=lambda: (btn_equal()))
-button_clear = Button(root, text="C", width=5, height=3, command=clearInput())
+button_mod = Button(root, text="%", width=5, height=3, command=lambda: btn_click("%"))
+button_sum = Button(root, text="=", width=5, height=6, command=lambda: btn_equal())
+button_clear = Button(root, text="C", width=5, height=3, command=lambda: clearInput())
 
 button1.grid(row=4, column=0)
 button2.grid(row=4, column=1)
